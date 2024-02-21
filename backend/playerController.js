aea9c0c734e149c0fb55c01a0c3b642d911e4bef
norrico31 <norricobiason31@gmail.com>
2024-02-21 16:28:22 +0800

    removing .env

--- backend/playerController.js
+++ backend/playerController.js
@@ -1,10 +1,26 @@
 import PlayerModel from './playerModel.js'
 
-export const getPlayers = async () => {
+export const getAllPlayers = async (req, res) => {
     try {
         const players = await PlayerModel.find({})
-        return players
+        return res.json(players)
     } catch (error) {
         return error
+    }
+}
+
+export const getPlayers = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        return error
+    }
+}
+
+export const updatePlayers = async (req, res) => {
+    try {
+        
+    } catch (error) {
+            return error
     }
 }
